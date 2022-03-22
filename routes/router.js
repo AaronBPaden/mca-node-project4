45b429a9625bebde49510290bae0ebec23db35af
aaronbpaden <aaronbpaden@gmail.com>
2022-03-22 10:04:28 -0500

    Implement initial router.js

---
+++ routes/router.js
@@ -0,0 +1,19 @@
+"use strict";
+const express = require('express');
+const router = express.Router();
+
+router.use(express.static('public'));
+
+router.get('*', (req, res) => {
+	switch(req.url) {
+		case '/favicon.ico':
+			res.end();
+		default:
+			res.status(404).render('pages/404', {
+				title: '404 Error',
+				name: '404 Error!',
+			});
+	}
+});
+
+module.exports = router;
